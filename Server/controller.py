6563dce66749f4327ce232facba6ab89b132da61
AlexCrosby <AlexCrosby@live.co.uk>
2021-01-09 16:22:42 +0000

    Fixed map

--- Server/controller.py
+++ Server/controller.py
@@ -72,4 +72,4 @@ class Controller:
 
     @staticmethod
     def map(x, in_min, in_max, out_min, out_max):
-        return int(((x - in_min) * (out_max - out_min)) / ((in_max - in_min) + out_min))
+        return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
